3e6a366097e770d8bf07cdcc25307ad68f8e0481
江昆 <jxq1690251766@gmail.com>
2020-02-09 13:01:37 +0800

    chore:添加webview在dialog的测试。

--- app/src/main/java/com/appframe/webview/TestWebViewActivity.java
+++ app/src/main/java/com/appframe/webview/TestWebViewActivity.java
@@ -2,6 +2,10 @@ package com.appframe.webview;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 import com.appframe.R;
 import com.appframe.framework.activity.AFBaseActivity;
@@ -14,6 +18,7 @@ import com.appframe.framework.webview.AFWebView;
 
 public class TestWebViewActivity extends AFBaseActivity {
     private AFWebView webView;
+    private Button mBtnDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,6 +28,20 @@ public class TestWebViewActivity extends AFBaseActivity {
         webView = findViewById(R.id.web_view);
         webView.loadUrl("https://yuncaiweb.ycaibaba.com/home?token=F8A0B1921ED95543791B22DED0203CFC&version=2.6.2");
 //        webView.loadUrl("http://www.baidu.com");
+
+        mBtnDialog = findViewById(R.id.btn_dialog);
+        mBtnDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(TestWebViewActivity.this)
+                        .setTitle("")
+                        .setView(R.layout.activity_test_web_view)
+                        .show();
+
+                AFWebView afWebView = alertDialog.findViewById(R.id.web_view);
+                afWebView.loadUrl("http://www.baidu.com");
+            }
+        });
     }
 
     @Override
